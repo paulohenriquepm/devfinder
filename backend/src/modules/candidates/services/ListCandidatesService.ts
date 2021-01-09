ba459fbfc54737bc1fc9467a177a5a215bcb19b5
paulohenriquepm <paulo.henriquepm@outlook.com>
2021-01-09 19:25:13 -0300

    finish ListCandidatesService

--- backend/src/modules/candidates/services/ListCandidatesService.ts
+++ backend/src/modules/candidates/services/ListCandidatesService.ts
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
+interface IRequest {
+  experience: any;
+  city: any;
+  technologies: any;
+}
+
 interface ITechnology {
   name: string;
   is_main_tech: boolean;
@@ -12,16 +18,81 @@ interface ICandidate {
   technologies: ITechnology[];
 }
 
-interface IResponse {
-  candidates: ICandidate[];
-}
-
 export default class ListCandidatesService {
-  public async execute(): Promise<ICandidate[]> {
+  public async execute({
+    experience,
+    city,
+    technologies,
+  }: IRequest): Promise<ICandidate[]> {
     const response = await axios.get(process.env.API_URL || '');
 
     const { candidates } = response.data;
 
-    return candidates;
+    let filteredCandidates: ICandidate[] = [];
+    let ids: Array<number> = [];
+
+    filteredCandidates = candidates.filter(
+      (candidate: ICandidate) =>
+        candidate.experience === experience &&
+        candidate.city === city &&
+        candidate.technologies.filter(
+          tech =>
+            technologies.includes(tech.name) && tech.is_main_tech === true,
+        ).length === technologies.length,
+    );
+
+    if (filteredCandidates.length !== 5) {
+      ids = filteredCandidates.map((candidate: ICandidate) => candidate.id);
+
+      const relatedCandidates = candidates.filter(
+        (candidate: ICandidate) =>
+          !ids.includes(candidate.id) &&
+          candidate.experience === experience &&
+          candidate.city === city &&
+          candidate.technologies.filter(tech =>
+            technologies.includes(tech.name),
+          ).length === technologies.length,
+      );
+
+      filteredCandidates = filteredCandidates.concat(relatedCandidates);
+      ids = ids.concat(
+        relatedCandidates.map((candidate: ICandidate) => candidate.id),
+      );
+    }
+
+    if (filteredCandidates.length !== 5) {
+      const relatedCandidates = candidates.filter(
+        (candidate: ICandidate) =>
+          !ids.includes(candidate.id) &&
+          candidate.city === city &&
+          candidate.technologies.filter(tech =>
+            technologies.includes(tech.name),
+          ).length === technologies.length,
+      );
+
+      filteredCandidates = filteredCandidates.concat(relatedCandidates);
+      ids = ids.concat(
+        relatedCandidates.map((candidate: ICandidate) => candidate.id),
+      );
+    }
+
+    if (filteredCandidates.length !== 5) {
+      const relatedCandidates = candidates.filter(
+        (candidate: ICandidate) =>
+          !ids.includes(candidate.id) &&
+          candidate.technologies.filter(tech =>
+            technologies.includes(tech.name),
+          ).length === technologies.length,
+      );
+
+      filteredCandidates = filteredCandidates.concat(relatedCandidates);
+      ids = ids.concat(
+        relatedCandidates.map((candidate: ICandidate) => candidate.id),
+      );
+    }
+
+    filteredCandidates = filteredCandidates.slice(0, 5);
+
+    return filteredCandidates;
   }
 }
