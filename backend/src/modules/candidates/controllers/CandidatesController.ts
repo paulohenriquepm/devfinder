eac2c18d132d667eee446d8d685806266e08dc27
paulohenriquepm <paulo.henriquepm@outlook.com>
2021-01-09 19:25:22 -0300

    finish CandidatesController

--- backend/src/modules/candidates/controllers/CandidatesController.ts
+++ backend/src/modules/candidates/controllers/CandidatesController.ts
@@ -4,9 +4,17 @@ import ListCandidatesService from '../services/ListCandidatesService';
 
 export default class CandidatesController {
   public async index(request: Request, response: Response): Promise<Response> {
+    const { experience, uf, city, technologies } = request.query;
+
+    const formattedCity = `${city} - ${uf}`;
+
     const listCandidates = new ListCandidatesService();
 
-    const candidates = await listCandidates.execute();
+    const candidates = await listCandidates.execute({
+      experience,
+      city: formattedCity,
+      technologies,
+    });
 
     return response.json(candidates);
   }
