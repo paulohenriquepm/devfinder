9a30494f7f9e4cf99d0b715c88b64e0822dbd79a
paulohenriquepm <paulo.henriquepm@outlook.com>
2021-01-09 19:24:57 -0300

    import spinner css

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap');
+  @import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
   * {
     margin: 0;
